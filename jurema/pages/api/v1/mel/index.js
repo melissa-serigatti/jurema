e48666454de5a5752d23ce2868a275e6c5a150ad
Melissa <melissaaraujoseigatti@gmail.com>
2025-04-20 19:46:57 +0000

    estudando

---
+++ jurema/pages/api/v1/mel/index.js
@@ -0,0 +1,5 @@
+async function mel(req, res){
+    res.status(200).json({status: "ok"})
+}
+
+export default mel;
---
+++ jurema/pages/index.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1><h1>Esse é o site da mel</h1></h1>
+  );
+}
