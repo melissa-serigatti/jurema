ad552c8713f0c9fb705920a592457a73a4d7840e
Melissa <melissaaraujoseigatti@gmail.com>
2025-05-11 16:38:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
+    <>
     <h1>Oi Mãe feliz dia das mães te amo❤️❤️❤️😙</h1>
+    <h1>oi Yara feia feliz aniversário meu amor ❤️🥳🎉</h1>
+    </>
   );
 }
+
